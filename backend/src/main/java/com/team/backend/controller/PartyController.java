28ebaf63ffd27f37ec256871ab9c115c16408203
yangyu-huang <1849588816@qq.com>
2021-04-27 01:24:18 +0800

    生成controller层相关类

---
+++ backend/src/main/java/com/team/backend/controller/PartyController.java
@@ -0,0 +1,21 @@
+package com.team.backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author yangyu
+ * @since 2021-04-26
+ */
+@RestController
+@RequestMapping("/party")
+public class PartyController {
+
+}
+
