f7fb792b368a0c50a6d26995b0a2ae23298c7a74
yangyu-huang <1849588816@qq.com>
2021-04-27 01:20:20 +0800

    新增web过滤器配置

---
+++ backend/src/main/java/com/team/backend/config/CorsFilter.java
@@ -0,0 +1,32 @@
+package com.team.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : yangyu
+ * @ClassName : com.team.backend.config.CorsFilter
+ * @Description : Cors配置类
+ * @date : 2021-04-22 23:39:42
+ * Copyright  2021 user. All rights reserved.
+ */
+
+@WebFilter(filterName = "CorsFilter ")
+@Configuration
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
+}
+
