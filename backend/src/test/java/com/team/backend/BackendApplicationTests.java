e1dc4b2de40d315f60719ed3bb718644b4e3f98f
yangyu-huang <1849588816@qq.com>
2021-04-27 01:18:04 +0800

    生成Spring Boot框架

---
+++ backend/src/test/java/com/team/backend/BackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team.backend;
+
+import com.team.backend.mapper.AdminMapper;
+import com.team.backend.model.Admin;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
