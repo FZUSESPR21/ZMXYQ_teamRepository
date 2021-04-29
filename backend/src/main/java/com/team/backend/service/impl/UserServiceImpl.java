ccd5459f07cb3f4cea521d4905977e8c96746a08
yangyu-huang <1849588816@qq.com>
2021-04-29 12:10:10 +0800

    完成上传照片service层代码, fixed #20

--- backend/src/main/java/com/team/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import com.team.backend.model.User;
 import com.team.backend.mapper.UserMapper;
 import com.team.backend.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import java.io.File;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +19,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+  @Autowired
+  UserMapper userMapper;
+
+  // 上传身份验证图片
+  public String identifyImg(File file) {
+    //等待后续until类中处理图片方法写好后再做修改
+    return "";
+  }
 }
