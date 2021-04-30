e8af896bdae4882a0edfb069f67ade549b47c87b
Tars <SETars@outlook.com>
2021-04-30 23:31:30 +0800

    - 实现PostService中获取帖子发布者展示信息方法  * 年龄信息未处理

--- backend/src/main/java/com/team/backend/service/PostService.java
+++ backend/src/main/java/com/team/backend/service/PostService.java
@@ -2,6 +2,7 @@ package com.team.backend.service;
 
 import com.team.backend.model.Post;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.team.backend.model.User;
 
 /**
  * <p>
@@ -9,8 +10,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * </p>
  *
  * @author yangyu
+ * @author Tars
  * @since 2021-04-28
  */
 public interface PostService extends IService<Post> {
+
+  /**
+   * 获取发帖人的信息展示数据
+   *
+   * @param publisherId the publisher id 发贴用户ID
+   * @return the post publisher 发帖人信息，包含username,birthday,isGraduated,
+   *        province,city,school,userIconUrl
+   */
+  User getPostPublisher(Long publisherId);
 
 }
