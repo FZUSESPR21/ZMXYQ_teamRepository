81a1a1171898b1365c4a1a4ff11e983e93650ef9
ccrjbcai <1126774947@qq.com>
2021-04-30 13:31:11 +0800

    fixed  #40

--- backend/src/main/java/com/team/backend/controller/AdminController.java
+++ backend/src/main/java/com/team/backend/controller/AdminController.java
@@ -1,8 +1,13 @@
 package com.team.backend.controller;
 
 
+import com.team.backend.util.Response;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,12 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
  * 前端控制器
  * </p>
  *
- * @author yangyu
+ * @author ccreater
  * @since 2021-04-28
  */
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
+  @GetMapping("/test")
+  public Response greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    Map<String,Object> data=new LinkedHashMap<>();
+    Map<String,Object> data2=new LinkedHashMap<>();
+    data2.put("fuck","fuck");
+    data.put("name",data2);
+    return new Response(0, name,data);
+  }
 }
 
