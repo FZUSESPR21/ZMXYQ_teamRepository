9471d26c6d6f2e8880f0d708e121d823dce67398
ccrjbcai <1126774947@qq.com>
2021-05-07 21:26:43 +0800

    User鉴权接口

--- backend/src/main/java/com/team/backend/filter/UserAuthFilter.java
+++ backend/src/main/java/com/team/backend/filter/UserAuthFilter.java
@@ -1,6 +1,7 @@
 package com.team.backend.filter;
 
 import com.team.backend.model.Admin;
+import com.team.backend.model.User;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,23 +16,27 @@ import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author : ccreater
- * @ClassName : com.team.backend.Filter.AdminAuthFilter
+ * @ClassName : com.team.backend.filter.UserAuthFilter
  * @Description : 类描述
- * @date : 2021-05-04 22:04 Copyright  2021 ccreater. All rights reserved.
+ * @date : 2021-05-07 21:04 Copyright  2021 ccreater. All rights reserved.
  */
-
-@WebFilter(urlPatterns = "/admin/*", filterName = "adminAuthFilter")
-public class AdminAuthFilter implements Filter {
+@WebFilter(urlPatterns = "/*", filterName = "userAuthFilter")
+public class UserAuthFilter implements Filter {
   @Value("${server.servlet.context-path}")
   String contextValue;
-
+  @Value("${test.istest}")
+  int isTest;
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
       FilterChain filterChain) throws IOException, ServletException {
+    if(isTest==1){
+      filterChain.doFilter(servletRequest, servletResponse);
+      return;
+    }
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     HttpSession session = request.getSession();
     HttpServletResponse response = (HttpServletResponse) servletResponse;
-    String [] noAuth = new String[]{"/admin/login","/admin/register","/admin/changepsw"};
+    String [] noAuth = new String[]{"/user/upload/img","/user/login"};
     String uri = request.getRequestURI();
     for(String path : noAuth){
       if(uri.equals(contextValue+path)){
@@ -40,7 +45,7 @@ public class AdminAuthFilter implements Filter {
       }
     }
     //need admin auth
-    if(isAdmin(session)){
+    if(isLogin(session)){
       filterChain.doFilter(servletRequest, servletResponse);
     }else{
       response.sendError(401);
@@ -48,9 +53,9 @@ public class AdminAuthFilter implements Filter {
     }
   }
 
-  public boolean isAdmin(HttpSession session){
-    Admin admin = (Admin) session.getAttribute("admin");
-    if(admin == null){
+  public boolean isLogin(HttpSession session){
+    User user = (User) session.getAttribute("user");
+    if(user == null){
       return false;
     }else{
       return true;
