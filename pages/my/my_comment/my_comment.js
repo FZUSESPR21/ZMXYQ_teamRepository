52182ac429078d5359ce93bb66a5df85dc562a61
camocd <6410942@qq.com>
2021-05-14 12:14:18 +0800

    feat:添加贴文评论和组局评论列表查询

--- pages/my/my_comment/my_comment.js
+++ pages/my/my_comment/my_comment.js
@@ -5,14 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    postCommentsList:[],
+    partyCommentsList:[],
   },
 
+
+  getPostComments(){
+    let that = this;
+    wx.request({
+      url:"http://localhost:8088/api/user/postcomment/list",
+      success(res){
+        console.log(res);
+        if(res.data.code === 200){
+          that.setData({
+            postCommentsList:res.data.data
+          })
+        }
+      }
+    })
+  },
+
+  getPartyComments(){
+    let that = this;
+    wx.request({
+      url:"http://localhost:8088/api/user/partycomment/list",
+      success(res){
+        console.log(res);
+        if(res.data.code === 200){
+          that.setData({
+            partyCommentsList:res.data.data
+          })
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getPostComments()
+    this.getPartyComments()
   },
 
   /**
