3c791524705ecab90f03c8b9a8cdf4f92c74e3a6
yangyu-huang <1849588816@qq.com>
2021-06-07 21:26:01 +0800

    完成清空私聊列表、删除私聊列表中的某一项、用户发送消息、用户接受消息接口的service、controller层代码

--- backend/src/main/java/com/team/backend/service/impl/PrivateChatServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/impl/PrivateChatServiceImpl.java
@@ -1,6 +1,8 @@
 package com.team.backend.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.team.backend.exception.ExceptionInfo;
 import com.team.backend.mapper.UserMapper;
 import com.team.backend.model.PrivateChat;
@@ -34,6 +36,7 @@ public class PrivateChatServiceImpl extends ServiceImpl<PrivateChatMapper, Priva
   @Autowired
   PrivateChatMapper privateChatMapper;
 
+  // 获取用户私聊列表
   public Result<List<Map<String, Object>>> listChat(Long id) {
 
     Result<List<Map<String, Object>>> result = new Result<>();
@@ -66,6 +69,135 @@ public class PrivateChatServiceImpl extends ServiceImpl<PrivateChatMapper, Priva
     result.setCode(ExceptionInfo.valueOf("OK").getCode());
     result.setMessage(ExceptionInfo.valueOf("OK").getMessage());
     result.setData(chatList);
+    return result;
+  }
+
+  // 清空用户私聊列表
+  public Result<Integer> clearList(Long id) {
+
+    Result<Integer> result = new Result<>();
+
+    QueryWrapper<PrivateChat> wrapper = new QueryWrapper<>();
+    wrapper.eq("from_id", id);
+
+    result.setCode(ExceptionInfo.valueOf("OK").getCode());
+    result.setMessage(ExceptionInfo.valueOf("OK").getMessage());
+    result.setData(privateChatMapper.delete(wrapper));
+    return result;
+  }
+
+
+  // 删除私聊列表中的某一项
+  public Result<Integer> deleteOne(Long id, Long dialogUserId) {
+
+    Result<Integer> result = new Result<>();
+
+    if (dialogUserId == null) {
+      result.setCode(ExceptionInfo.valueOf("USER_DIALOG_ID_NULL").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_DIALOG_ID_NULL").getMessage());
+      return result;
+    }
+
+    QueryWrapper<PrivateChat> wrapper = new QueryWrapper<>();
+    wrapper.eq("from_id", id)
+        .eq("to_id", dialogUserId);
+
+    result.setCode(ExceptionInfo.valueOf("OK").getCode());
+    result.setMessage(ExceptionInfo.valueOf("OK").getMessage());
+    result.setData(privateChatMapper.delete(wrapper));
+    return result;
+  }
+
+  // 用户发送消息
+  public Result<Integer> sendMessage(Long id, Long userIdTo, String content) {
+
+    Result<Integer> result = new Result<>();
+
+    if (userIdTo == null) {
+      result.setCode(ExceptionInfo.valueOf("USER_DIALOG_ID_NULL").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_DIALOG_ID_NULL").getMessage());
+      return result;
+    }
+
+    if (content == null || content == "") {
+      result.setCode(ExceptionInfo.valueOf("USER_SEND_MESSAGE_NULL").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_SEND_MESSAGE_NULL").getMessage());
+      return result;
+    }
+
+    PrivateChat privateChat = new PrivateChat();
+    privateChat.setFromId(id);
+    privateChat.setToId(userIdTo);
+    privateChat.setMessage(content);
+
+    result.setCode(ExceptionInfo.valueOf("OK").getCode());
+    result.setMessage(ExceptionInfo.valueOf("OK").getMessage());
+    result.setData(privateChatMapper.insert(privateChat));
+    return result;
+  }
+
+  // 用户接收消息
+  public Result<List<Map<String, Object>>> receiveMessage(Long id, Long toId, Integer pageNum,
+      Integer pageSize) {
+
+    Result<List<Map<String, Object>>> result = new Result<>();
+    List<Map<String, Object>> messageList = new LinkedList<>();
+
+    if (toId == null) {
+      result.setCode(ExceptionInfo.valueOf("USER_DIALOG_ID_NULL").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_DIALOG_ID_NULL").getMessage());
+      return result;
+    }
+
+    if (pageNum == null) {
+      result.setCode(ExceptionInfo.valueOf("USER_PAGE_NUM_NULL").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_PAGE_NUM_NULL").getMessage());
+      return result;
+    }
+
+    if (pageNum < 0) {
+      result.setCode(ExceptionInfo.valueOf("USER_PAGE_NUM_BELOW0").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_PAGE_NUM_BELOW0").getMessage());
+      return result;
+    }
+
+    if (pageSize == null) {
+      result.setCode(ExceptionInfo.valueOf("USER_PAGE_SIZE_NULL").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_PAGE_SIZE_NULL").getMessage());
+      return result;
+    }
+
+    if (pageSize < 0) {
+      result.setCode(ExceptionInfo.valueOf("USER_PAGE_SIZE_BELOW0").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_PAGE_SIZE_BELOW0").getMessage());
+      return result;
+    }
+
+    QueryWrapper<PrivateChat> wrapper = new QueryWrapper<>();
+    wrapper.eq("from_id", id).eq("to_id", toId)
+        .or(wq -> wq.eq("from_id", toId).eq("to_id", id))
+        .orderByDesc("gmt_create");
+
+    IPage<PrivateChat> page = new Page<>(pageNum, pageSize);
+    page = privateChatMapper.selectPage(page, wrapper);
+    List<PrivateChat> privateChatList = page.getRecords();
+
+    for (PrivateChat privateChat : privateChatList) {
+      Map<String, Object> message = new HashMap<>();
+      message.put("time", privateChat.getGmtCreate());
+      message.put("content", privateChat.getMessage());
+      if (privateChat.getFromId().equals(id)) {
+        message.put("isFromMe", 1);
+      } else {
+        message.put("isFromMe", 0);
+      }
+
+      messageList.add(message);
+    }
+
+    result.setCode(ExceptionInfo.valueOf("OK").getCode());
+    result.setMessage(ExceptionInfo.valueOf("OK").getMessage());
+    result.setData(messageList);
     return result;
   }
 
