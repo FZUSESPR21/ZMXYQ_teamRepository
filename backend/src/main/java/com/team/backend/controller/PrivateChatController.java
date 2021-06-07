393fa47c4e03eb37dc879ab7e19658be39500c3a
yangyu-huang <1849588816@qq.com>
2021-06-07 18:45:18 +0800

    完成获取私聊用户列表service、controller代码

--- backend/src/main/java/com/team/backend/controller/PrivateChatController.java
+++ backend/src/main/java/com/team/backend/controller/PrivateChatController.java
@@ -1,6 +1,14 @@
 package com.team.backend.controller;
 
 
+import com.team.backend.exception.ExceptionInfo;
+import com.team.backend.model.Result;
+import com.team.backend.model.User;
+import com.team.backend.service.impl.PrivateChatServiceImpl;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-04-28
  */
 @RestController
-@RequestMapping("${server.api-path}/private-chat")
+@RequestMapping("${server.api-path}/message/chat")
 public class PrivateChatController {
 
+  User user = null;
+
+  @Autowired
+  PrivateChatServiceImpl privateChatService;
+
+  @PostMapping("/list")
+  public Result<List<Map<String, Object>>> listChat()
+  {
+
+    if (user == null) {
+      Result<List<Map<String, Object>>> result = new Result<>();
+      result.setCode(ExceptionInfo.valueOf("USER_NOT_LOGIN").getCode());
+      result.setMessage(ExceptionInfo.valueOf("USER_NOT_LOGIN").getMessage());
+      return result;
+    }
+
+    return privateChatService.listChat(user.getId());
+  }
 }
 
--- backend/src/main/java/com/team/backend/service/impl/PrivateChatServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/impl/PrivateChatServiceImpl.java
@@ -1,9 +1,19 @@
 package com.team.backend.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.team.backend.exception.ExceptionInfo;
+import com.team.backend.mapper.UserMapper;
 import com.team.backend.model.PrivateChat;
 import com.team.backend.mapper.PrivateChatMapper;
+import com.team.backend.model.Result;
+import com.team.backend.model.User;
 import com.team.backend.service.PrivateChatService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,5 +27,46 @@ import org.springframework.stereotype.Service;
 @Service
 public class PrivateChatServiceImpl extends ServiceImpl<PrivateChatMapper, PrivateChat> implements
     PrivateChatService {
+
+  @Autowired
+  UserMapper userMapper;
+
+  @Autowired
+  PrivateChatMapper privateChatMapper;
+
+  public Result<List<Map<String, Object>>> listChat(Long id) {
+
+    Result<List<Map<String, Object>>> result = new Result<>();
+    List<Map<String, Object>> chatList = new LinkedList<>();
+
+    QueryWrapper<PrivateChat> wrapper = new QueryWrapper<>();
+    wrapper.select("to_id")
+        .eq("from_id", id)
+        .groupBy("to_id");
+    List<Map<String, Object>> maps = privateChatMapper.selectMaps(wrapper);
+
+    for (Map<String, Object> objectMap : maps) {
+      Long toId = (Long) objectMap.get("to_id");
+      Map<String, Object> map = new HashMap<>();
+      map.put("dialogUserId", toId);
+      User user = userMapper.selectById(toId);
+      map.put("dialogUserName", user.getUsername());
+      map.put("iconUrl", user.getUserIconUrl());
+      QueryWrapper<PrivateChat> chatWrapper = new QueryWrapper<>();
+      chatWrapper.eq("from_id", id)
+          .eq("to_id", toId)
+          .orderByDesc("gmt_create");
+      List<PrivateChat> privateChatList = privateChatMapper.selectList(chatWrapper);
+      PrivateChat privateChat = privateChatList.get(0);
+      map.put("latestTime", privateChat.getGmtCreate());
+      map.put("latestMessage", privateChat.getMessage());
+      chatList.add(map);
+    }
+
+    result.setCode(ExceptionInfo.valueOf("OK").getCode());
+    result.setMessage(ExceptionInfo.valueOf("OK").getMessage());
+    result.setData(chatList);
+    return result;
+  }
 
 }
