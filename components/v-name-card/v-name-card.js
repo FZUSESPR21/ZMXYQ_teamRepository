7cb8b367c36f52b489baddff98093058b0d6b57d
此号慢热型 <1241859128@qq.com>
2021-06-18 11:43:36 +0800

    拉黑私聊逻辑
    拉黑私聊逻辑

--- components/v-name-card/v-name-card.js
+++ components/v-name-card/v-name-card.js
@@ -104,41 +104,54 @@ Component({
        * 拉黑用户
        * 
        */
-      console.log(this.data.publisherMsg)
+      //console.log(this.data.publisherMsg)
       let uid = this.data.userId
-      wx.showModal({
-        title: '提示',
-        content: 'QAQ您确定要拉黑该用户吗',
-        success (res) {
-          if (res.confirm) {
-            let jsonStr = '{"userIdTo":' + uid + ',"isBlock":'+ 1 +'}';
-            let jsonValue = JSON.parse(jsonStr);
-            request({
-              url : app.globalData.baseUrl + "/api/block",
-              method : "post",
-              Headers : {
-                'content-type': 'application/json'
-              },
-              data : jsonValue,
-              success:function(res){
-                wx.showToast({
-                  title: '成功拉黑',
-                  icon:'success',
-                  duration:1000
-                })
-              },
-              fail:function(res){
-                wx.showToast({
-                  title: '拉黑失败',
-                  icon:'error',
-                  duration:1000
-                })
-              }
-            })
-          } else if (res.cancel) {
+      let mid = wx.getStorageSync('openid').id
+      console.log(uid)
+      console.log(mid)
+      if(uid==mid){
+        wx.showModal({
+          title: '警告',
+          content:'你拉黑自己干嘛(╯▔皿▔)╯',
+          showCancel : false,
+          confirmText : '下次还敢'
+        })
+      }
+      else{
+        wx.showModal({
+          title: '提示',
+          content: 'QAQ您确定要拉黑该用户吗',
+          success (res) {
+            if (res.confirm) {
+              let jsonStr = '{"userIdTo":' + uid + ',"isBlock":'+ 1 +'}';
+              let jsonValue = JSON.parse(jsonStr);
+              request({
+                url : app.globalData.baseUrl + "/api/block",
+                method : "post",
+                Headers : {
+                  'content-type': 'application/json'
+                },
+                data : jsonValue,
+                success:function(res){
+                  wx.showToast({
+                    title: '成功拉黑',
+                    icon:'success',
+                    duration:1000
+                  })
+                },
+                fail:function(res){
+                  wx.showToast({
+                    title: '拉黑失败',
+                    icon:'error',
+                    duration:1000
+                  })
+                }
+              })
+            } else if (res.cancel) {
+            }
           }
-        }
-      })
+        })
+      }
     },
     goToChat:function(e){
       /**
@@ -146,9 +159,22 @@ Component({
        * 用户私聊
        * 
        */
-      wx.navigateTo({
-        url: '/pages/message/chat/chat?userid='+this.data.userId+'&iconurl='+this.data.publisherMsg.iconUrl
-      })
+      let uid = this.data.userId
+      let mid = wx.getStorageSync('openid').id
+      if(uid==mid)
+      {
+        wx.showModal({
+          title: '同情',
+          content:'再寂寞也不要和自己私聊哦',
+          showCancel : false,
+          confirmText : '我哭了'
+        })
+      }
+      else{
+        wx.navigateTo({
+          url: '/pages/message/chat/chat?userid='+this.data.userId+'&iconurl='+this.data.publisherMsg.iconUrl
+        })
+      }
     }
   },
   lifetimes:{
